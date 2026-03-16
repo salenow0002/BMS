84f831184defcc8da6e044a8344db1c98b8afe68
salenow0002 <salenow0002@gmail.com>
2026-03-16 15:59:42 +0530

    Update home.tsx

--- src/home.tsx
+++ src/home.tsx
@@ -70,12 +70,12 @@ function Home() {
       date: "01-Apr-26",
       time: "7:30 PM",
       team1: {
-        name: "Delhi Capitals",
-        logo: "https://upload.wikimedia.org/wikipedia/en/2/2f/Delhi_Capitals.svg",
-      },
-      team2: {
         name: "Lucknow Super Giants",
         logo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Lucknow_Super_Giants_Logo.svg/5000px-Lucknow_Super_Giants_Logo.svg.png",
+      },
+      team2: {
+        name: "Delhi Capitals",
+        logo: "https://upload.wikimedia.org/wikipedia/en/2/2f/Delhi_Capitals.svg",
       },
       venue: "Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium, Lucknow",
     },
@@ -85,12 +85,12 @@ function Home() {
       date: "02-Apr-26",
       time: "7:30 PM",
       team1: {
-        name: "Sunrisers Hyderabad",
-        logo: "https://upload.wikimedia.org/wikipedia/en/5/51/Sunrisers_Hyderabad_Logo.svg",
-      },
-      team2: {
         name: "Kolkata Knight Riders",
         logo: "https://upload.wikimedia.org/wikipedia/en/4/4c/Kolkata_Knight_Riders_Logo.svg",
+      },
+      team2: {
+        name: "Sunrisers Hyderabad",
+        logo: "https://upload.wikimedia.org/wikipedia/en/5/51/Sunrisers_Hyderabad_Logo.svg",
       },
       venue: "Eden Gardens, Kolkata",
     },
@@ -115,12 +115,12 @@ function Home() {
       date: "04-Apr-26",
       time: "3:30 PM",
       team1: {
-        name: "Mumbai Indians",
-        logo: "https://upload.wikimedia.org/wikipedia/en/c/cd/Mumbai_Indians_Logo.svg",
-      },
-      team2: {
         name: "Delhi Capitals",
         logo: "https://upload.wikimedia.org/wikipedia/en/2/2f/Delhi_Capitals.svg",
+      },
+      team2: {
+        name: "Mumbai Indians",
+        logo: "https://upload.wikimedia.org/wikipedia/en/c/cd/Mumbai_Indians_Logo.svg",
       },
       venue: "Arun Jaitley Stadium, Delhi",
     },
@@ -145,12 +145,12 @@ function Home() {
       date: "05-Apr-26",
       time: "3:30 PM",
       team1: {
-        name: "Lucknow Super Giants",
-        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Lucknow_Super_Giants_Logo.svg/5000px-Lucknow_Super_Giants_Logo.svg.png",
-      },
-      team2: {
         name: "Sunrisers Hyderabad",
         logo: "https://upload.wikimedia.org/wikipedia/en/5/51/Sunrisers_Hyderabad_Logo.svg",
+      },
+      team2: {
+        name: "Lucknow Super Giants",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Lucknow_Super_Giants_Logo.svg/5000px-Lucknow_Super_Giants_Logo.svg.png",
       },
       venue: "Rajiv Gandhi International Stadium, Hyderabad",
     },
@@ -187,12 +187,12 @@ function Home() {
       date: "07-Apr-26",
       time: "7:30 PM",
       team1: {
-        name: "Mumbai Indians",
-        logo: "https://upload.wikimedia.org/wikipedia/en/c/cd/Mumbai_Indians_Logo.svg",
-      },
-      team2: {
         name: "Rajasthan Royals",
         logo: "https://upload.wikimedia.org/wikipedia/en/5/5c/This_is_the_logo_for_Rajasthan_Royals%2C_a_cricket_team_playing_in_the_Indian_Premier_League_%28IPL%29.svg",
+      },
+      team2: {
+        name: "Mumbai Indians",
+        logo: "https://upload.wikimedia.org/wikipedia/en/c/cd/Mumbai_Indians_Logo.svg",
       },
       venue: "ACA Stadium, Guwahati",
     },
@@ -215,12 +215,12 @@ function Home() {
       date: "09-Apr-26",
       time: "7:30 PM",
       team1: {
-        name: "Lucknow Super Giants",
-        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Lucknow_Super_Giants_Logo.svg/5000px-Lucknow_Super_Giants_Logo.svg.png",
-      },
-      team2: {
         name: "Kolkata Knight Riders",
         logo: "https://upload.wikimedia.org/wikipedia/en/4/4c/Kolkata_Knight_Riders_Logo.svg",
+      },
+      team2: {
+        name: "Lucknow Super Giants",
+        logo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Lucknow_Super_Giants_Logo.svg/5000px-Lucknow_Super_Giants_Logo.svg.png",
       },
       venue: "Eden Gardens, Kolkata",
     },
@@ -271,12 +271,12 @@ function Home() {
       date: "12-Apr-26",
       time: "3:30 PM",
       team1: {
-        name: "Gujarat Titans",
-        logo: "https://upload.wikimedia.org/wikipedia/en/0/09/Gujarat_Titans_Logo.svg",
-      },
-      team2: {
         name: "Lucknow Super Giants",
         logo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Lucknow_Super_Giants_Logo.svg/5000px-Lucknow_Super_Giants_Logo.svg.png",
+      },
+      team2: {
+        name: "Gujarat Titans",
+        logo: "https://upload.wikimedia.org/wikipedia/en/0/09/Gujarat_Titans_Logo.svg",
       },
       venue: "Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium, Lucknow",
     },
